d011d3cb289db3ed5c17056b64bfe5c1aca55af0
zahraNejati <nejatizahra4@gmail.com>
2023-11-18 14:45:49 +0330

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/addNewNote";
 import NoteList from "./components/NoteList";
@@ -6,15 +6,31 @@ import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
   const [sortBy, setSortBy] = useState("");
-
+  const noteReducer = (state, { type, payload }) => {
+    switch (type) {
+      case "ADDNOTE":
+        return [...state, payload];
+      case "DELETENOTE":
+        return state.filter((note) => note.id !== payload);
+      case "COMPLETENOTE":
+        return state.map((note) =>
+          note.id === payload ? { ...note, complete: !note.complete } : note
+        );
+      default:
+        return state;
+    }
+  };
+  const [notes, dispatch] = useReducer(noteReducer, []);
   const handleAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "ADDNOTE", payload: newNote });
   };
   const handleDeleteNote = (id) => {
-    const filteredNotes = notes.filter((note) => note.id !== id);
-    setNotes(filteredNotes);
+    // const filteredNotes = notes.filter((note) => note.id !== id);
+    // setNotes(filteredNotes);
+    dispatch({ type: "DELETENOTE", payload: id });
   };
   const handleCompleteNote = (e) => {
     const noteId = Number(e.target.value);
@@ -22,11 +38,12 @@ const App = () => {
     //   note.id === noteId ? { ...note, complete: !note.complete } : note
     // );
     // setNotes(newNotes);
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, complete: !note.complete } : note
-      )
-    );
+    // setNotes((prevNotes) =>
+    //   prevNotes.map((note) =>
+    //     note.id === noteId ? { ...note, complete: !note.complete } : note
+    //   )
+    // );
+    dispatch({ type: "COMPLETENOTE", payload: noteId });
   };
 
   return (
